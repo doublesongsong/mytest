edd6e49ca807724b88a7159083238ed7656a517a
liruihao <liruihao@baidu.com>
2015-12-24 11:30:43 +0800

    add a source file

---
+++ a.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+ > File Name: a.cpp
+ > Author: liruihao
+ > Mail: liruihao@baidu.com 
+ > Created Time: 四 12/24 11:30:26 2015
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<cmath>
+#include<utility>
+using namespace std;
+#define REP(i,n) for(int i = 0; i < (n); ++i)
+#define FOR(i,l,r) for(int i = (l); i <= (r); ++i)
+#define DWN(i,r,l) for(int i = (r); i >= (l); --i)
+
+int main()
+{
+	freopen("data.in","r",stdin);
+	return 0;
+}
+
